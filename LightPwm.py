9e4cfa0c9692274e41b5dd94373f93a615018a11
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-08-29 16:03:24 +0200

    Use RPi.GPIO instead of wiringpi

--- LightPwm.py
+++ LightPwm.py
@@ -1,17 +1,17 @@
-import wiringpi
+import pigpio
+import atexit
 
 # Control light with hardware pwm
 class LightPwm:
 
     def __init__(self, gpio_pin):
         self.pin = gpio_pin
-        wiringpi.wiringPiSetupGpio()
-        wiringpi.pinMode( gpio_pin, wiringpi.PWM_OUTPUT )
-        wiringpi.pwmSetRange( 100 )
-        wiringpi.pwmWrite( gpio_pin, 0 )
-        
+        self.pi = pigpio.pi()
+        self.pi.hardware_PWM(self.pin, 800, 0)
+        self.pi.set_PWM_range(self.pin, 100)
+
     def __del__(self):
-        pass
+        self.pi.stop()
 
     def set_value(self, value):
         if value > 100:
@@ -19,7 +19,7 @@ class LightPwm:
         elif value < 0:
             value = 0
 
-        wiringpi.pwmWrite( self.pin, value )
+        self.pi.set_PWM_dutycycle(self.pin, value )
 
     def on(self):
         self.set_value(100)
@@ -28,3 +28,19 @@ class LightPwm:
     def off(self):
         self.set_value(0)
 
+
+if __name__ == '__main__':
+    import time
+
+    light = LightPwm(13)
+
+    print("===== LightPWM Main =====")
+
+    while(True):
+        for dc in range(0, 101, 10):
+            light.set_value(dc)
+            time.sleep(0.5)
+
+        for dc in range(100, -1, -10):
+            light.set_value(dc)
+            time.sleep(0.5)
